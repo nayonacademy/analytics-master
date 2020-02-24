66bbd609e36c53eee982a704426a79f29dd0e003
nayonacademy <si.nayon@gmail.com>
2020-02-24 17:43:30 +0600

    Initial commit

---
+++ analytics.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Analytics Master
+ * Plugin URI: http://wordpress.org/plugins/analytics-master
+ * Description: Best analytics for wordpress website and woocommerce
+ * Version: 1.0.0
+ * Author: Shariful Islam Nayon
+ * Author URI: https://nayon.net
+ * Text Domain: analytics-master
+
+*/
+
